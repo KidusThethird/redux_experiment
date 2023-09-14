3ab12bf1b899cd0dad19f002d971df84aa83e278
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-09-14 13:22:35 +0300

    starting file updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-green-600">This is a page</h1>
+    </div>
+  );
+}
+
+export default App;
